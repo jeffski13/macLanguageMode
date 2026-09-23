3b5c44d1cbc5ca36cf3f9815e3a0d0146fa99275
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-09-23 15:04:05 -0500

    bug fix for english keyboard

--- englishMode.zsh
+++ englishMode.zsh
@@ -3,4 +3,4 @@
 # open google translate to EN to JP
 open -a "Google Chrome" "https://translate.google.co.jp/?hl=en&sl=en&tl=ja&op=translate"
 # change keyboard to EN
-/opt/homebrew/bin/macism com.apple.com.apple.keylayout.US
+/opt/homebrew/bin/macism com.apple.keylayout.US
