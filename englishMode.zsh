edc1e0d2a39dcdf995278b9c5eb117d4fc878965
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-09-23 14:53:06 -0500

    base commit for shell scripts that change language

---
+++ englishMode.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# open google translate to EN to JP
+open -a "Google Chrome" "https://translate.google.co.jp/?hl=en&sl=en&tl=ja&op=translate"
+# change keyboard to EN
+/opt/homebrew/bin/macism com.apple.com.apple.keylayout.US
---
+++ japaneseMode.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# open google translate to EN to JP
+open -a "Google Chrome" "https://translate.google.co.jp/?hl=en&sl=ja&tl=en&op=translate"
+# change keyboard to JP
+/opt/homebrew/bin/macism com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese
